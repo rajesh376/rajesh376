e3fe9ae0128179e66986f08e178b2090dadf6d08
Rajesh <74093715+rajesh376@users.noreply.github.com>
2023-02-04 16:50:43 +0530

    First Commit
    Practise

---
+++ index.js
@@ -0,0 +1,54 @@
+function myFunction() {
+    var x = document.getElementById('box');
+ if (x.style.display === "none"){
+    x.style.display = "block"
+ }else{
+    x.style.display = "none";
+    alert('if you hide navlinks make sure for bigger screen you must unhide links navlinks')
+ }
+}
+
+function doFunction () {
+   var x = document.getElementById('div1');
+if (x.style.display === "none"){
+   x.style.display = "flex"
+}else{
+   x.style.display = "none";
+  
+}
+
+}
+
+function doFunction1 () {
+   var x = document.getElementById('div2');
+   if (x.style.display === "none"){
+      x.style.display = "flex"
+   }else{
+      x.style.display = "none";
+  
+}
+
+}
+
+function doFunction2 () {
+   var x = document.getElementById('div3');
+   if (x.style.display === "none"){
+      x.style.display= "flex"
+   }else{
+      x.style.display = "none";
+  
+}
+
+}
+
+function doFunction3 () {
+   var x = document.getElementById('div4');
+   if (x.style.display === "none"){
+      x.style.display = "flex"
+   }else{
+      x.style.display = "none";
+  
+}
+
+}
+
